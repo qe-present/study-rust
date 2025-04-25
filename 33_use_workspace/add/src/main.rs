4645c4208e3cda9f6b0f7f33e18428a0801bce77
qe-present <2664481691@qq.com>
2025-04-25 17:12:35 +0800

    add

---
+++ 33_use_workspace/add/src/main.rs
@@ -0,0 +1,9 @@
+///
+/// 运行cargo run -p add
+use add_one;
+
+
+fn main() {
+    let a=add_one::add_one(1);
+    println!("{}",a)
+}
---
+++ 34_use_Box/src/main.rs
@@ -0,0 +1,26 @@
+/*
+BOX智能指针，
+引用进借用数据，但不是拥有数据
+比如 Box、Rc、Ref、RefMut、RefCell
+Box特点
+允许将数据存储在Heap上
+ 
+ */
+enum List{
+    Cons(i32, Box<List>),
+    Nil,
+}
+fn main() {
+    let l=List::Cons(5, Box::new(List::Cons(10, Box::new(List::Nil))));
+}
+#[cfg(test)]
+mod test {
+    #[test]
+    fn question_1(){
+        let mut n=1;
+        let b=Box::new(&mut n);
+        **b+=1;
+        println!("{}",n)
+    }
+    
+}
